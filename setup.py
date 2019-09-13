66748920ec8380561eab917c8d261dbc9518e490
Ioannis Mavroukakis <imavroukakis@gmail.com>
2019-09-13 11:39:50 +0100

    update setup

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot-alert-msteams',
-      version='0.0.1',
+      version='0.0.2',
       description='An msteams alert plugin for Cabot by Arachnys',
-      author='mk',
-      author_email='maxim.kur@exponential.com',
+      author='im',
+      author_email='imavroukakis@gmail.com',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url = 'https://github.com/imavroukakis/cabot-alert-msteams/tarball/0.0.1',
+      download_url='https://github.com/imavroukakis/cabot-alert-msteams/tarball/0.0.2',
      )
